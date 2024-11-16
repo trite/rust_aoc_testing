cd9b9760ce9ce3676b7593c56f827f24aafdfe93
Paul Brunner <trite@users.noreply.github.com>
2024-11-15 19:18:54 -0700

    day 9 setup, quick template update

--- src/year2015/day09.rs
+++ src/year2015/day09.rs
@@ -7,12 +7,12 @@ pub fn part_2(_input: &str) -> i32 {
 }
 
 generate_tests!(
-    -2015,          // year
-    -1,             // day
+    2015,           // year
+    9,              // day
     part_1,         // part 1 function
     part_2,         // part 2 function
-    vec![("", -1)], // part 1 examples
-    vec![("", -1)], // part 2 examples
+    vec![("London to Dublin = 464\nLondon to Belfast = 518\nDublin to Belfast = 141", 605)], // part 1 examples
+    vec![], // part 2 examples
     -1,             // part 1 expected
     -1              // part 2 expected
 );
